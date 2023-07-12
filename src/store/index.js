5774a7c9e0a1cbf18fa298977f14670d44151620
chan9yu <wry5533@gmail.com>
2023-07-12 23:58:22 +0900

    feat: celar

--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { writable, get } from "svelte/store";
-
-export const todos = writable([])
-export function saveStorage() {
-  localStorage.setItem('todos', JSON.stringify(get(todos)))
-}
--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-const autoPreprocess = require('svelte-preprocess')
-
-module.exports = {
-  preprocess: autoPreprocess({
-    scss: {
-      prependData: '@import "./src/scss/main.scss";'
-    }
-  })
-}
