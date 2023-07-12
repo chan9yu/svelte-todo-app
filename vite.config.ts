8329e76be0b2c7e80c701a5d522a44e33a60b2d7
chan9yu <wry5533@gmail.com>
2023-07-13 00:26:12 +0900

    feat: update project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+  plugins: [svelte()],
+  server: {
+    port: 3040
+  }
+})
