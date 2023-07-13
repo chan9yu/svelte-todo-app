2f5af47dc6d0e95973580b2baeeb27ea71904631
chan9yu <wry5533@gmail.com>
2023-07-13 13:18:21 +0900

    feat: 프로젝트 정리

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as TodoHeader } from './TodoHeader.svelte';
+export { default as TodoInfo } from './TodoInfo.svelte';
+export { default as TodoItem } from './TodoItem.svelte';
+export { default as TodoList } from './TodoList.svelte';
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,16 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	settings: {
+		svelte: {
+			ignoreWarnings: [
+				'@typescript-eslint/no-unsafe-assignment',
+				'@typescript-eslint/no-unsafe-member-access'
+			],
+			compileOptions: {}
+		}
+	}
+};
