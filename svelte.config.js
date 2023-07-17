b7816e0a104aacf4a4d1a64484379f05cf821ab7
chan9yu <wry5533@gmail.com>
2023-07-17 16:49:35 +0900

    chore: update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-	preprocess: vitePreprocess(),
-	settings: {
-		svelte: {
-			ignoreWarnings: [
-				'@typescript-eslint/no-unsafe-assignment',
-				'@typescript-eslint/no-unsafe-member-access'
-			],
-			compileOptions: {}
-		}
-	}
+	preprocess: vitePreprocess()
 };
