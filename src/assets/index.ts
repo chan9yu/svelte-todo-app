91080b82ba0ec220248d986b46e6e21a6aeb7ed5
chan9yu <wry5533@gmail.com>
2023-07-22 22:55:59 +0900

    feat: added assets

---
+++ src/assets/index.ts
@@ -0,0 +1,10 @@
+export { default as stickyNotesPng } from './images/sticky-notes.png';
+
+export { default as addSvg } from './svgs/add.svg';
+export { default as articleSvg } from './svgs/article.svg';
+export { default as checkSvg } from './svgs/check.svg';
+export { default as deleteSvg } from './svgs/delete.svg';
+export { default as editSvg } from './svgs/edit.svg';
+export { default as githubSvg } from './svgs/github.svg';
+export { default as saveSvg } from './svgs/save.svg';
+export { default as uncheckSvg } from './svgs/uncheck.svg';
